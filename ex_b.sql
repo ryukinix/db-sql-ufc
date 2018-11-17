f3572bd37004e16d34d111009d705286ad14d60c
Manoel Vilela <manoel_vilela@engineer.com>
2018-11-17 17:18:16 -0300

    Add solution for ex_b.sql

--- ex_b.sql
+++ ex_b.sql
@@ -1,2 +1,6 @@
 -- b) Encontre os nomes e cidades de residência de todos
---    os empregados que trabalham para o Banco do Brasil;
+--    os empregados que trabalham para o Banco do Brazil;
+
+SELECT nome_empregado, cidade
+FROM empregado NATURAL JOIN trabalha_para
+WHERE nome_companhia = 'Banco do Brasil';
