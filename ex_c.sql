7ef9b1477c562b949616630460c49708504c7948
Manoel Vilela <manoel_vilela@engineer.com>
2018-11-17 17:18:45 -0300

    Add solution for ex_c.sql

--- ex_c.sql
+++ ex_c.sql
@@ -1,3 +1,7 @@
 -- c) Encontre os nomes, ruas e cidades de residência de todos os
 --    empregados que trabalham para o Banco do Brasil e que ganham mais
 --    de que R$ 10.000,00;
+
+SELECT nome_empregado, rua, cidade
+FROM empregado NATURAL JOIN trabalha_para
+WHERE nome_companhia = 'Banco do Brasil' AND salario > 10000.00;
