c222950d572595e8480f966054ec148c7b4552d1
Manoel Vilela <manoel_vilela@engineer.com>
2018-11-17 17:48:23 -0300

    Add solution ex_g.sql

--- ex_g.sql
+++ ex_g.sql
@@ -1,2 +1,8 @@
 -- g) Encontre os nomes de todos os empregados que ganham mais de que
 -- todos os empregados do Banco Safra;
+
+SELECT nome_empregado
+FROM trabalha_para
+WHERE salario > ALL(SELECT salario
+                    FROM trabalha_para
+                    WHERE nome_companhia = 'Banco Safra')
