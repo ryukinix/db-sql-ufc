749bff437c0a54c376f33e88e8667bf9fe993840
Manoel Vilela <manoel_vilela@engineer.com>
2018-11-17 19:06:57 -0300

    Add solution for ex_k.sql

--- ex_k.sql
+++ ex_k.sql
@@ -1,2 +1,8 @@
 -- k) Encontre os nomes de todos os empregados que trabalham para,
 -- pelo menos, duas companhias.
+
+
+SELECT nome_empregado
+FROM trabalha_para
+GROUP BY nome_empregado
+HAVING count(nome_companhia) >= 2;
