47d7dbb51e670e955151d31ad90a76c7c6867e60
Manoel Vilela <manoel_vilela@engineer.com>
2018-11-17 17:23:43 -0300

    Add solution for ex_d.sql

--- ex_d.sql
+++ ex_d.sql
@@ -1,2 +1,8 @@
 -- d) Encontre os nomes de todos os empregados neste banco de dados
 -- que moram na mesma cidade da companhia para a qual eles trabalham;
+
+SELECT e.nome_empregado
+FROM empregado as e
+NATURAL JOIN trabalha_para as t
+NATURAL JOIN companhia as c
+WHERE e.cidade = c.cidade;
