3ab96609bf1dcc08ed4f8bdfc6ed22ab0ca60967
Manoel Vilela <manoel_vilela@engineer.com>
2018-11-17 17:44:32 -0300

    Add solution ex_f.sql

--- ex_f.sql
+++ ex_f.sql
@@ -1,2 +1,8 @@
 -- f) Encontre os nomes de todos os empregados neste banco de dados
 -- que NÃO trabalham para o Banco do Brasil;
+
+SELECT nome_empregado
+FROM trabalha_para
+WHERE nome_empregado not in (SELECT nome_empregado
+                             FROM trabalha_para
+                             WHERE nome_companhia = 'Banco do Brasil');
